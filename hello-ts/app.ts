a243336397f643f98ba970c0f5bb2d4e63eabf1e
cdpjenkins <cdpjenkins@gmail.com>
2022-03-06 19:58:23 +0000

    Add a few more graphical things

--- hello-ts/app.ts
+++ hello-ts/app.ts
@@ -2,6 +2,12 @@ console.log('I am an initial TS app!!!1');
 
 var c = document.getElementById("myCanvas") as HTMLCanvasElement;
 var ctx = c.getContext("2d");
+
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, 400, 400);
+
+ctx.strokeStyle = "#FFFFFF"
+
 ctx.moveTo(0, 0);
 ctx.lineTo(400, 400);
 ctx.stroke();
@@ -9,3 +15,7 @@ ctx.stroke();
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
+
+ctx.font = "30px Arial";
+ctx.fillStyle = "#FFFFFF";
+ctx.fillText("Hello TypeScript!!!1", 10, 50);
