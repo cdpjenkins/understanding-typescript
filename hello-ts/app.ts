444c022675b0deee63fa831c5c1b627848f1f3b4
cdpjenkins <cdpjenkins@gmail.com>
2022-03-06 19:43:17 +0000

    Add some canvas shenanigans

--- hello-ts/app.ts
+++ hello-ts/app.ts
@@ -1,1 +1,11 @@
 console.log('I am an initial TS app!!!1');
+
+var c = document.getElementById("myCanvas") as HTMLCanvasElement;
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(400, 400);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
