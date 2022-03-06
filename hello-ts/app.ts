8e1fa27bea92eb856e9bd26f6442dd587d88f67e
cdpjenkins <cdpjenkins@gmail.com>
2022-03-06 20:19:37 +0000

    Add some dots

--- hello-ts/app.ts
+++ hello-ts/app.ts
@@ -4,10 +4,9 @@ var c = document.getElementById("myCanvas") as HTMLCanvasElement;
 var ctx = c.getContext("2d");
 
 ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, 400, 400);
+ctx.fillRect(0, 0, c.width, c.height);
 
 ctx.strokeStyle = "#FFFFFF"
-
 ctx.moveTo(0, 0);
 ctx.lineTo(400, 400);
 ctx.stroke();
@@ -19,3 +18,19 @@ ctx.stroke();
 ctx.font = "30px Arial";
 ctx.fillStyle = "#FFFFFF";
 ctx.fillText("Hello TypeScript!!!1", 10, 50);
+
+setInterval(drawDots, 500)
+
+function drawDots() {
+    console.log("drawDots")
+
+    for (var i = 0; i < 100; i++) {
+        drawDot(ctx, Math.random() * c.width, Math.random() * c.height);
+    }
+}
+
+function drawDot(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    var brightness = Math.random() * 256
+    ctx.fillStyle = "rgb(" + brightness + "," + brightness + "," + brightness + ")";
+    ctx.fillRect(x, y, 1, 1);
+}
