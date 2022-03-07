2777b3b1dd36e7466e6b64acd7acd6e6293d0933
cdpjenkins <cdpjenkins@gmail.com>
2022-03-07 21:14:32 +0000

    Use keys to fly around starfield

--- hello-ts/app.ts
+++ hello-ts/app.ts
@@ -1,8 +1,18 @@
 const numStars = 10000;
 
-console.log('I am an initial TS app!!!1');
+let vx = 0;
+let vy = 0;
+
+let keysDown = {};
 
 var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+document.addEventListener('keydown', function(e) {
+    keysDown[e.key] = true;
+}, false);
+document.addEventListener('keyup', function(e) {
+    keysDown[e.key] = false;
+}, false);
+
 var ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "#000000";
@@ -29,9 +39,33 @@ function openFullscreen() {
 
 function tick() {
     stars.forEach(function(star, i, stars) {
-        star.x = (star.x - 1 / star.distance);
+
+        if (keysDown['a']) {
+            vx += 0.00001;
+        }
+        if (keysDown['w']) {
+            vy += 0.00001;
+        }
+        if (keysDown['d']) {
+            vx -= 0.00001;
+        }
+        if (keysDown['s']) {
+            vy -= 0.00001;
+        }
+
+        star.x = (star.x + vx / star.z);
+        star.y = (star.y + vy / star.z);
         if (star.x < 0) {
             star.x += canvas.width;
+        }
+        if (star.x >= canvas.width) {
+            star.x -= canvas.width;
+        }
+        if (star.y < 0) {
+            star.y += canvas.height;
+        }
+        if (star.y >= canvas.height) {
+            star.y -= canvas.height;
         }
     });
 
@@ -42,10 +76,10 @@ function makeStars() {
     let stars = [];
 
     for (var i = 0; i < numStars; i++) {
-        var star = {
+        const star = {
             "x": Math.random() * canvas.width,
             "y": Math.random() * canvas.height,
-            "distance": Math.random() * 10 + 1
+            "z": Math.random() * 10 + 1
         }
         stars.push(star);
     }
@@ -54,8 +88,6 @@ function makeStars() {
 }
 
 function drawStars() {
-    console.log("drawDots")
-
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -65,14 +97,12 @@ function drawStars() {
 }
 
 function drawStar(ctx: CanvasRenderingContext2D, dot) {
-    var brightness = 255.0 / (dot.distance)
-    // ctx.fillRect(dot.x, dot.y, 2, 2);
+    drawDot(dot.x, dot.y, 255.0 / (dot.z), Math.min(1/dot.z, 3));
+}
 
-    const r = Math.min(1/dot.distance, 3);
-
+function drawDot(x: number, y: number, brightness: number, r: number) {
     ctx.beginPath();
-    ctx.arc(dot.x, dot.y, r, 0, 2*Math.PI);
+    ctx.arc(x, y, r, 0, 2*Math.PI);
     ctx.fillStyle = `rgb(${brightness},${brightness},${brightness})`;
     ctx.fill();
-  
 }
