30f33a5636020d2e6ebdef121540f392d2ccaab8
Chris Jenkins <cdpjenkins@gmail.com>
2023-10-15 00:44:38 +0100

    Client does GETs and POSTs

--- hello-node/src/client.ts
+++ hello-node/src/client.ts
@@ -1,6 +1,26 @@
 import axios from 'axios';
 
-axios("http://localhost:3000/todos")
-    .then(res => {
-        console.log(res.data);
+addTODO("TODO number one");
+addTODO("the second TODO");
+addTODO("et cetera");
+
+getAllTODOs();
+
+function getAllTODOs() {
+    axios("http://localhost:3000/todos")
+        .then(res => {
+            console.log(res.data);
+        });
+}
+
+function addTODO(message: string) {
+    axios.post(
+        "http://localhost:3000/todos",
+        {
+            text: message
+        }
+    ).then(res => {
+        console.log(res.status);
     });
+}
+
