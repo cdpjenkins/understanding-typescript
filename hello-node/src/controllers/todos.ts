67cd9c017c19d03f0ed1779361e3f46aad38ad52
Chris Jenkins <cdpjenkins@gmail.com>
2023-10-14 16:19:44 +0100

    Beginnings of a little Express app
    Diverging slightly from the course but not *too* much...

---
+++ hello-node/src/controllers/todos.ts
@@ -0,0 +1,40 @@
+import { RequestHandler, Request, Response, NextFunction } from "express";
+
+import { Todo } from "../models/todosModel";
+
+const todos: Todo[] = [];
+
+let lastId: number = 0;
+
+function allocateId(): string {
+    return (++lastId).toString();
+}
+
+export const createTodo: RequestHandler = ( req, res, next) => {
+    const text = (req.body as {text: string}).text;
+    const newTodo = new Todo(allocateId(), text);
+
+    console.log(newTodo);
+
+    todos.push(newTodo);
+
+    return res.status(201).json(
+        {
+            message: "Created a TODO",
+            createdTodo: newTodo
+        }
+    )
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+    // const id = req.query["id"];
+    // const todo = todos.find( (entry) => { entry.id == id });
+    // 
+    // if (todo != undefined) {
+        // return res.status(200).json(todo);
+    // } else {
+        // return res.status(404);
+    // }
+
+    return res.status(200).json(todos);
+}
--- hello-node/src/main.ts
+++ hello-node/src/main.ts
@@ -1,1 +1,17 @@
-console.log("prrrrrrrrrr!");
+import express from "express";
+import { json } from "body-parser";
+
+import router from "./routes/todos";
+
+const app = express();
+
+app.use(json());
+
+app.use("/todos", router);
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(500).json({message: err.message});
+});
+
+app.listen(3000);
+
