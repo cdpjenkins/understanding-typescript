192eb627336351ba763340a6645f0e1651008e88
Chris Jenkins <cdpjenkins@gmail.com>
2023-10-17 18:51:34 +0100

    Return HTML instead of plain text

--- hello-node/src/plain-server.ts
+++ hello-node/src/plain-server.ts
@@ -2,14 +2,38 @@
 
 import http from "http";
 
-const server = http.createServer( (req, res) => {
-        console.log("received a request!!!1");
-        
-        
-        res.setHeader("Content-type", "text/plain");
-        res.write("Hello, world!!!1\r\n");
+let visitorNumber = 0;
+
+const server = http.createServer( (req, res) => {    
+        res.setHeader("Content-type", "text/html");
+        res.write(`
+        <html>
+            <body>
+                <h1>Hello World!!!1</h1>
+            </body>
+
+            <p>
+                This is a page returned from Node.
+            </p>
+
+            <p>
+                You are visitor number ${getNextVisitorNumber()}.
+            </p>
+
+            <p>
+                Please visit again soon!
+            </p>
+        </html>`);
         res.end();
     }
 )
+
+// Note that the visitor number appears to go up by 2 each time when you hit this from Chrome (and likely other browsers)
+// because Chrome sends two requests, one for "/" and one for "favicon.ico".
+function getNextVisitorNumber() {
+    console.log(visitorNumber);
+
+    return ++visitorNumber;
+}
 
 server.listen(3001);
