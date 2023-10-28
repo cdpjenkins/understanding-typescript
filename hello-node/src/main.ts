28dc59c5e4cb10c4d1578e58ce49f57de7f73c5e
Chris Jenkins <cdpjenkins@gmail.com>
2023-10-28 09:45:32 +0100

    Add a 404 page
    Re lecture 67.

--- hello-node/src/main.ts
+++ hello-node/src/main.ts
@@ -13,6 +13,10 @@ app.use("/todos", todosRouter);
 app.use("/admin", adminRouter);
 app.use("/shop", shopRouter);
 
+app.use((req, res, next) => {
+    res.status(404).send("<html><body><h1>Not found!!!1</body></html>\n");
+});
+
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     res.status(500).json({message: err.message});
 });
