f130aac50787981f560e3ca0961550521c8a7131
Chris Jenkins <cdpjenkins@gmail.com>
2024-02-24 15:33:37 +0000

    Max's updated HTML and CSS

--- hello-node/src/main.ts
+++ hello-node/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(json());
 
-app.use("/public", express.static( path.join(rootDir, "public")));
+app.use(express.static( path.join(rootDir, "public")));
 
 app.use("/todos", todosRouter);
 app.use("/admin", adminRouter);
